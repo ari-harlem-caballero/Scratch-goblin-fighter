952c2f865bb9c9ceb8f14f89b2d64303bcc3c6f8
Ari Harlem-Caballero <ari.bash.harlem@gmail.com>
2021-12-09 17:07:56 -0800

    added DOM and let state in JS

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
 // import functions and grab DOM elements
-
+const form = document.querySelector('form');
+const defeatedNumber = document.querySelector('#defeated-number');
+const defeatedList = document.querySelector('.defeated-list');
+const playerHPElem = document.querySelector('.player-hp');
+const playerImg = document.querySelector('#player-img');
+const goblinList = document.querySelector('.current-goblins');
 // let state
+let playerHP = 10;
+let goblinInfo = [
+    { name: 'Michael', hp: 2 },
+    { name: 'Dwight', hp: 4 }
+];
+let defeatedCount = 0;
 
 // set event listeners 
   // get user input
